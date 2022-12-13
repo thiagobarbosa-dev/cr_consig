8922a9052a943cb3438b4969525aa56157669d71
Thiago Barbosa <thiagobarbosa.dev@icloud.com>
2022-12-13 09:58:33 -0300

    Removed SSHKit from deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,8 +39,6 @@ set :keep_releases, 5
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-SSHKit.config.command_map.prefix[:puma].push("bundle exec")
-SSHKit.config.command_map.prefix[:pumactl].push("bundle exec")
 
 set :migration_role, :app
 
