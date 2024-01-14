57cdae1f4b7c52f9959d5b65dacb871da81f3aef
Thiago Barbosa <thiagobarbosa.dev@icloud.com>
2024-01-14 17:47:46 -0300

    24-update-readme-and-seeds * Updated Readme, seeds and gitignore.
    closes #24

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# load 'db/seeds/users_seeds.rb'
-# load 'db/seeds/customers_seeds.rb'
-# load 'db/seeds/products_seeds.rb'
-# load 'db/seeds/shipping_types_seeds.rb'
+load 'db/seeds/users_seeds.rb'
+load 'db/seeds/customers_seeds.rb'
+load 'db/seeds/products_seeds.rb'
+load 'db/seeds/shipping_types_seeds.rb'
--- db/seeds/users_seeds.rb
+++ db/seeds/users_seeds.rb
@@ -1,8 +1,8 @@
 User.create!(name: "Administrador",
               login: "admin",
               email: "admin@localhost",
-              password: "120988",
-              password_confirmation: "120988")
+              password: "123321",
+              password_confirmation: "123321")
 # 50.times do |n|
 #   name = Faker::Name.name
 #   login = Faker::Internet.unique.email
